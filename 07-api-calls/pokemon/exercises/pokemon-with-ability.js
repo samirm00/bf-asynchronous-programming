8d59d975c608c71812615edf69bf26917892e7de
samirm00 <sammoham.be@gmail.com>
2023-08-16 00:42:56 +0200

    api-calls

--- 07-api-calls/pokemon/exercises/pokemon-with-ability.js
+++ 07-api-calls/pokemon/exercises/pokemon-with-ability.js
@@ -10,33 +10,33 @@ import { ORIGIN } from '../config.js';
  * @throws {Error} HTTP error! status: {number}.
  */
 export const pokemonWithAbility = async (ability = '') => {
-    // --- generate and declare your resource's URL ---
-    // docs: https://pokeapi.co/docs/v2#abilities
-    const URL = `${ORIGIN}/ability/${ability}`;
+  // --- generate and declare your resource's URL ---
+  // docs: https://pokeapi.co/docs/v2#abilities
+  const URL = `${ORIGIN}/ability/${ability}`;
 
-    // --- fetch the API data (this works!) ---
-    const encodedURL = encodeURI(URL);
-    const response = await fetch(encodedURL);
+  // --- fetch the API data (this works!) ---
+  const encodedURL = encodeURI(URL);
+  const response = await fetch(encodedURL);
 
-    // --- throw an error if the response is not ok (this works!) ---
-    if (!response.ok) {
-        const message = response.statusText
-            ? `${response.status}: ${response.statusText}\n-> ${URL}`
-            : `HTTP error! status: ${response.status}\n-> ${URL}`;
-        throw new Error(message);
-    }
+  // --- throw an error if the response is not ok (this works!) ---
+  if (!response.ok) {
+    const message = response.statusText
+      ? `${response.status}: ${response.statusText}\n-> ${URL}`
+      : `HTTP error! status: ${response.status}\n-> ${URL}`;
+    throw new Error(message);
+  }
 
-    /* --- parse the data if the response was ok (this works!) ---*/
-    const data = await response.json();
+  /* --- parse the data if the response was ok (this works!) ---*/
+  const data = await response.json();
 
-    // --- process the fetched data (if necessary) ---
-    //  you do not need to use `await` below this comment
-    //  you can refactor this to a separate logic function and test it
-    const pokemon = data.pokemon.map((poke) => ({
-        name: poke.pokemon.name,
-        url: poke.pokemon.url,
-    }));
+  // --- process the fetched data (if necessary) ---
+  //  you do not need to use `await` below this comment
+  //  you can refactor this to a separate logic function and test it
+  const pokemon = data.pokemon.map((poke) => ({
+    name: poke.pokemon.name,
+    url: poke.pokemon.url,
+  }));
 
-    // --- return the final data ---
-    return pokemon;
+  // --- return the final data ---
+  return pokemon;
 };
